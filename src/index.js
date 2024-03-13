5b28d7cad07a7e04fffeeae00fe547a418df0b93
Reza <79734970+ezazee@users.noreply.github.com>
2024-03-13 17:56:10 +0000

    fix: deleting annalytics

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { Analytics } from "@vercel/analytics/react"
 
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <Analytics />
   </React.StrictMode>,
   document.getElementById('root')
 );
